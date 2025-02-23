abecfba947452257fa368db1cae9b659d3ce8ce9
tim.sanders <tim.sanders@kinesso.com>
2025-02-23 08:20:06 +0000

    refactor connect_to_google_sheets function to use credentials dictionary and clean up commented code

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import pandas as pd
 import json
 
 # Function to authenticate and connect to Google Sheets
-def connect_to_google_sheets(json_keyfile, sheet_name, worksheet_name):
+def connect_to_google_sheets(credentials_dict, sheet_name, worksheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name).worksheet(worksheet_name)
     data = sheet.get_all_records()
@@ -41,28 +41,23 @@ def main():
                 st.error("Invalid username or password")
     else:
         st.title("Logged in")
-        # # Input fields for JSON keyfile, Google Sheets name, and worksheet name
-        # if os.getenv("IS_LOCAL"):
-        #     # credentials = "creds/gs_ts.json"
-        #     credentials = json.loads(st.secrets["gcp_service_account"])
-        # else:
-        #     credentials = json.loads(st.secrets["gcp_service_account"])
-                
-        # sheet_name = "Ski 25 - Via Lattea"
-        # worksheet_name = "test"
+        # Input fields for JSON keyfile, Google Sheets name, and worksheet name
+        credentials = st.secrets["gcp_service_account"]
+        sheet_name = "Ski 25 - Via Lattea"
+        worksheet_name = "test"
 
-        # if st.button("Load Data"):
-        #     try:
-        #         df = connect_to_google_sheets(credentials, sheet_name, worksheet_name)
-        #         st.write("Data from Google Sheets:")
-        #         st.dataframe(df)
-        #     except Exception as e:
-        #         st.error(f"Error: {e}")
+        if st.button("Load Data"):
+            try:
+                df = connect_to_google_sheets(credentials, sheet_name, worksheet_name)
+                st.write("Data from Google Sheets:")
+                st.dataframe(df)
+            except Exception as e:
+                st.error(f"Error: {e}")
         
-        # if st.button("Logout"):
-        #     st.session_state.logged_in = False
-        #     st.success("Logged out successfully!")
-        #     st.rerun()
+        if st.button("Logout"):
+            st.session_state.logged_in = False
+            st.success("Logged out successfully!")
+            st.rerun()
 
 if __name__ == "__main__":
     main()
